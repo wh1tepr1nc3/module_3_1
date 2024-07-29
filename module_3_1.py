59b294f8d0ca0101bccd8a877a357fd50ac5ea87
wh1tepr1nc3 <amamamania@mail.ru>
2024-07-29 20:27:33 +0500

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,23 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    count_calls()
+    return len(string), string.upper(), string.lower()
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    return string.upper() in [string.upper() for string in list_to_search]
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
